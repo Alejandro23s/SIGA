fb4d23f1ccbdf63f8c570bd2520688de16d4f925
Alejandro <alejandro.barajas.s01@gmail.com>
2026-08-03 01:56:11 -0600

    Configura API pública con Cloudflare Tunnel

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let state = {
     editingTramiteId: null
 };
 
-const API_URL = "https://somewhat-dictionary-reduces-newspaper.trycloudflare.com";
+const API_URL = " https://humor-advantages-undergraduate-gadgets.trycloudflare.com";
 
 // UI HELPERS
 function showToast(msg, icon = "fa-check-circle") {
